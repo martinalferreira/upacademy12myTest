746d2b763dc08a0ed71e7aa38f9e3e402deee9fe
martinalferreira <martina.lopes19@gmail.com>
2019-12-12 11:26:00 +0000

    desafio 4

--- Examples/src/pt/upacademy/desafios/desafio4/Desafio.java
+++ Examples/src/pt/upacademy/desafios/desafio4/Desafio.java
@@ -5,8 +5,17 @@ public class Desafio {
 	public static int[] ints(String string) {
 		
 		String[] splited = string.split(" ");
+		Integer result = new Integer[];
 		
-		return null;
+		for (int i = 0; i <= 9; i++) {
+				for (String index : splited) {
+					if (index.contains(String.valueOf(i))) {
+						result.add(Integer.parseInt(index + ","));
+					}
+				}
+		}
+		
+		return result;
 	}
 
 }
