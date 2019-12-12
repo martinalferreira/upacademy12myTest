946a9bca5c6999afba61e1e2dc4078d882b03f26
martinalferreira <martina.lopes19@gmail.com>
2019-12-12 15:12:04 +0000

    desafio 4 completed

--- Examples/src/pt/upacademy/desafios/desafio2/Desafio.java
+++ Examples/src/pt/upacademy/desafios/desafio2/Desafio.java
@@ -6,17 +6,13 @@ public class Desafio {
 		
 		String[] splitedWords = string.split(" ");
 		StringBuilder result = new StringBuilder ("");
-		int numOfWords = 0;
 		
 		for (int i = 0; i <= 9; i++) {
-			if (numOfWords < splitedWords.length) {
 				for (String word : splitedWords) {
 					if (word.contains(String.valueOf(i))) {
 						result.append(word + " ");
 					}
 				}
-				numOfWords ++;
-			}
 		}
 		return result.toString().trim();
 	}
--- Examples/src/pt/upacademy/desafios/desafio4/Desafio.java
+++ Examples/src/pt/upacademy/desafios/desafio4/Desafio.java
@@ -1,21 +1,32 @@
 package pt.upacademy.desafios.desafio4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Desafio {
 
 	public static int[] ints(String string) {
 		
+		if (string == "") {
+			return new int[]{};
+		}
+		
 		String[] splited = string.split(" ");
-		Integer result = new Integer[];
+		List<Integer> resultList = new ArrayList<Integer>();
 		
 		for (int i = 0; i <= 9; i++) {
-				for (String index : splited) {
-					if (index.contains(String.valueOf(i))) {
-						result.add(Integer.parseInt(index + ","));
+				for (String element : splited) {
+					if (element.equals(String.valueOf(i))) {
+						resultList.add(Integer.parseInt(element));
 					}
 				}
 		}
 		
-		return result;
+		int[] resultArray = new int[resultList.size()];
+		for (int i = 0; i < resultArray.length; i++) {
+			resultArray[i] = resultList.get(i);
+		}
+		return resultArray;
 	}
 
 }
